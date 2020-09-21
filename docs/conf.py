45c7e4971cb390074e6c9748ab031fbe3eb06506
Guido Kollerie <guido@kollerie.com>
2020-09-21 07:03:18 +0200

    Don't sort module members when using Sphinx autodoc.
    There is a certain order in each and every module. When all the
    members of a module are sorted, as happens in Sphinx by default, that
    order, and the logic it associated it to it, is lost.

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,8 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+autodoc_member_order = "bysource"
+
 todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
