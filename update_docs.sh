12a9e986eb7ba09ee7c52952c41a2aa63177acce
Hans Trompert <hans.trompert@surf.nl>
2025-04-09 09:40:48 +0200

    update the way docs are published

--- update_docs.sh
+++ update_docs.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# check if working tree is clean
+if git diff --quiet --exit-code
+then
+    : working tree is clean
+else
+    echo "working tree not clean, will not update docs"
+    exit 1
+fi
+
 # build the docs
 cd docs
 make clean
@@ -7,19 +16,20 @@ make html
 cd ..
 
 # stash
+git add docs/_build
 git stash
 
 # switch branches and pull the data we want
+set -e
 git checkout gh-pages
 rm -rf *
+git stash pop
 touch .nojekyll
 git checkout master .gitignore
-git checkout master docs/_build/html
 mv ./docs/_build/html/* ./
 rm -rf ./docs
 git add -A
-git commit -m "publish updated docs"
+git commit -m "publish updated docs" --no-verify
 git push origin gh-pages --force
 ## switch back
 git checkout master
-git stash pop
